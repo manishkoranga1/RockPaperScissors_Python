384e644d90c57c099845193e5bf5ffe5031fffe7
Manish <korangamanishsingh2002@gmail.com>
2023-01-07 15:02:39 +0530

    My first commit

---
+++ rock_paper_scissors.py
@@ -0,0 +1,56 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+item_name = ["Rock", "Paper", "Scissors"]
+
+user_choice = int(
+    input(
+        "What do you choose?\nType-\n0 for Rock,\n1 for Paper,\n2 for Scissors.\n"
+    ))
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you lose!")
+else:
+    print(f"You chose: {item_name[user_choice]}")
+    print(game_images[user_choice])
+
+    computer_choice = random.randint(0, 2)
+    print(f"Computer chose: {item_name[computer_choice]}")
+    print(game_images[computer_choice])
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw")
